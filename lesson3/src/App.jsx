2c1d705893d6ead3cae7ef9576796f93fc206cbd
nightingale16066 <nightingale16066@gmail.com>
2022-12-08 12:00:28 +0300

    fix LifeCycle

--- lesson3/src/App.jsx
+++ lesson3/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ComponentClass from './components/ClassComponent';
+// import ComponentClass from './components/ClassComponent';
+import LifeCycle from './components/LifeCycle';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='app'>
-        <ComponentClass min={1} max={10}/>
+        {/* <ComponentClass min={1} max={10}/> */}
+        <LifeCycle prop='METHED'/>
       </div>
     );
   }
--- lesson3/src/components/LifeCycle/LifeCycle.jsx
+++ lesson3/src/components/LifeCycle/LifeCycle.jsx
@@ -2,32 +2,101 @@ import React from 'react';
 import style from './LifeCycle.module.css';
 
 export class LifeCycle extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+
+    this.state = {
+      field: 0,
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('getDerivedStateFromProps');
+    return state;
+  }
+
+  static getDerivedStateFromError(err) {
+    return {
+      hasError: true,
+    };
+  }
+
+  componentDidCatch(err, errorInfo) {
+    // sendLog(errorInfo.componentStack);
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+
+    // setTimeout(() => {
+    //   this.setState(state => ({
+    //     field: state.field + 1,
+    //   }));
+    // }, 3000);
+
+    // document.addEventListener('scroll', this.handler);
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    console.log('shouldComponentUpdate');
+
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('getSnapshotBeforeUpdate');
+    return window.pageYOffset;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('componentDidUpdate');
+    window.scrollBy(0, -snapshot);
+  }
+
+  componentWillUnmount() {
+    // document.addEventListener('scroll', this.handler);
+  }
+
+  handler = () => {
+    this.setState(state => ({field: state.field + 1}));
+  };
+
   render() {
-    return (
-      <div>
-        <h1 className={style.title}>Жизненный цикл</h1>
+    console.log('render');
+    if (this.state.hasError) {
+      return <h1 className={style.title}>Error</h1>;
+    } else {
+      return (
+        <div>
+          <h1 className={style.title}>Жизненный цикл</h1>
 
-        <div className={style.container}>
-          <div>
-            <h2 className={style.title}>Типы</h2>
-            <ul className={style.list}>
-              <li>Монтирование</li>
-              <li>Обновление</li>
-              <li>Размонтирование</li>
-              <li>Ошибки</li>
-            </ul>
-          </div>
+          <div className={style.container}>
+            <div>
+              <h2 className={style.title}>Типы</h2>
+              <ul className={style.list}>
+                <li>Монтирование</li>
+                <li>Обновление</li>
+                <li>Размонтирование</li>
+                <li>Ошибки</li>
+              </ul>
+            </div>
 
-          <div className='stage'>
-            <h2 className={style.title}>Этапы</h2>
-            <ul className={style.list}>
-              <li>Render</li>
-              <li>Pre-commit</li>
-              <li>Commit</li>
-            </ul>
+            <div className='stage'>
+              <h2 className={style.title}>Этапы</h2>
+              <ul className={style.list}>
+                <li>Render</li>
+                <li>Pre-commit</li>
+                <li>Commit</li>
+              </ul>
+            </div>
           </div>
+          <button className={style.btn} onClick={this.handler}>
+            Click {this.state.field}
+          </button>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
